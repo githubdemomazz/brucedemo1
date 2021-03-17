e46c6e93d6a2eae10b91b281311a30a58d133e46
githubdemomazz <mazzucca12@gmail.com>
2021-03-16 20:32:56 -0400

    got the map working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 //function jjjdemo(){
 //	alert("demo javascript");
 //};
+
+//function initMap() {
+  //map = new google.maps.Map(document.getElementById("map"), {
+    //center: { lat: -34.397, lng: 150.644 },
+    //zoom: 8,
+ // });
+//}
