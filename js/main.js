1222598bd963dfa1de16702948e63145b9131b32
githubdemomazz <mazzucca12@gmail.com>
2021-03-21 14:55:37 -0400

    cleanupcomments2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-//function jjjdemo(){
-//	alert("demo javascript");
-//};
-
-//function initMap() {
-  //map = new google.maps.Map(document.getElementById("map"), {
-    //center: { lat: -34.397, lng: 150.644 },
-    //zoom: 8,
- // });
-//}
-
 
 $(document).ready(function(){
   $("p").click(function(){
